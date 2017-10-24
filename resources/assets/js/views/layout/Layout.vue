e30c8cd24e85faa80cf9ec4c765412e26710d742
sam <shaogen.chen@molbase.com>
2017-10-24 17:43:24 +0800

    支持对elasticsearch的测试

---
+++ resources/assets/js/views/layout/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="main-container">
+        <navbar></navbar>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import  {Navbar} from 'views/layout'
+
+    export default {
+        name: 'layout',
+        components: {
+            Navbar
+        }
+    }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+    .main-container {
+        min-height: 100%;
+        transition: margin-left 0.28s ease-out;
+    }
+
+</style>
