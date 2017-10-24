7206b3f1bec0e3ac31059179f90a0ea9d8daf66f
sam <shaogen.chen@molbase.com>
2017-10-24 17:42:52 +0800

    支持对elasticsearch的测试

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,20 +2,12 @@ let mix = require('laravel-mix');
 const path = require('path');
 
 mix.webpackConfig({
-    module: {
-        rules: [
-            // {
-            //     test: /\.css$/,
-            //     loader: "style-loader!css-loader"
-            // },
-        ]
-    },
     resolve: {
         alias: {
             '@': 'assets/js',
-            'components': path.resolve(__dirname, 'assets/js/components'),
-            'views': path.resolve(__dirname, 'assets/js/views'),
-            'router': path.resolve(__dirname, 'assets/js/router'),
+            'components': 'assets/js/components',
+            'views': 'assets/js/views',
+            'router': 'assets/js/router',
         },
         modules: [
             'node_modules',
@@ -37,6 +29,6 @@ mix.webpackConfig({
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    // .extract(['vue', 'vue-resource', 'lodash', 'jquery', 'element-ui'])
+// .extract(['vue', 'vue-resource', 'lodash', 'jquery', 'element-ui'])
     .sass('resources/assets/sass/app.scss', 'public/css')
     .version();
